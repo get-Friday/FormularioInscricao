848956485154ad0c1a6f6f3bd5b1a8cb7c876905
FridayAI <gabi.get@hotmail.com>
2022-06-23 14:20:16 -0300

    adding class FichaInscricao

---
+++ FormularioInscricao/FichaInscricao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class FichaInscricao
+{
+	public string Nome { get; set; }
+	public int Idade { get; set; }
+	public string Curso { get; set; }
+	public string Escolaridade { get; set; }
+	public string DataNascimento { get; set; }
+	public decimal? ValorCurso { get; set; }
+	public decimal? ValorDesconto { get; set; }
+	public decimal? ValorMulta { get; set; }
+
+	public FichaInscricao()
+	{
+	}
+}
