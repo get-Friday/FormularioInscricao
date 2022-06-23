bd32a63d6e7d939677d8dd0f11ef94fc54272795
FridayAI <gabi.get@hotmail.com>
2022-06-23 14:36:22 -0300

    Adicionar método CalcularIdade

--- FormularioInscricao/FichaInscricao.cs
+++ FormularioInscricao/FichaInscricao.cs
@@ -6,7 +6,7 @@ public class FichaInscricao
 	public int Idade { get; set; }
 	public string Curso { get; set; }
 	public string Escolaridade { get; set; }
-	public DateOnly DataNascimento { get; set; }
+	public DateTime DataNascimento { get; set; }
 	public decimal? ValorCurso { get; set; }
 	public decimal? ValorDesconto { get; set; }
 	public decimal? ValorMulta { get; set; }
@@ -15,13 +15,14 @@ public class FichaInscricao
 		string nome, 
 		string curso, 
 		string escolaridade, 
-		DateOnly dataNascimento,
+		DateTime dataNascimento,
 		decimal? valorCurso,
 		decimal? valorDescondo,
 		decimal? valorMulta
 		)
 	{
 		Nome = nome;
+		Idade = CalcularIdade(dataNascimento);
 		Curso = curso;
 		Escolaridade = escolaridade;
 		DataNascimento = dataNascimento;
@@ -29,4 +30,9 @@ public class FichaInscricao
 		ValorDesconto = valorDescondo;
 		ValorMulta = valorMulta;
 	}
+
+	private int CalcularIdade(DateTime DataNascimento)
+    {
+		return DataNascimento.Year - DateTime.Now.Year;
+    }
 }
