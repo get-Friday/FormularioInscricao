cd052dd3ce1bfde9fabed2464f9db97a6e48ded6
FridayAI <gabi.get@hotmail.com>
2022-06-23 14:31:28 -0300

    Populando o constructor

--- FormularioInscricao/FichaInscricao.cs
+++ FormularioInscricao/FichaInscricao.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿namespace FormularioInscricao;
 
 public class FichaInscricao
 {
@@ -6,12 +6,27 @@ public class FichaInscricao
 	public int Idade { get; set; }
 	public string Curso { get; set; }
 	public string Escolaridade { get; set; }
-	public string DataNascimento { get; set; }
+	public DateOnly DataNascimento { get; set; }
 	public decimal? ValorCurso { get; set; }
 	public decimal? ValorDesconto { get; set; }
 	public decimal? ValorMulta { get; set; }
 
-	public FichaInscricao()
+	public FichaInscricao(
+		string nome, 
+		string curso, 
+		string escolaridade, 
+		DateOnly dataNascimento,
+		decimal? valorCurso,
+		decimal? valorDescondo,
+		decimal? valorMulta
+		)
 	{
+		Nome = nome;
+		Curso = curso;
+		Escolaridade = escolaridade;
+		DataNascimento = dataNascimento;
+		ValorCurso = valorCurso;
+		ValorDesconto = valorDescondo;
+		ValorMulta = valorMulta;
 	}
 }
