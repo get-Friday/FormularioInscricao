06452b4dba640941f4a301a6cdc981359fc8a959
FridayAI <gabi.get@hotmail.com>
2022-06-27 19:31:48 -0300

    Editando window form

--- FormularioInscricao/Form1.Designer.cs
+++ FormularioInscricao/Form1.Designer.cs
@@ -28,12 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCurso = new System.Windows.Forms.TextBox();
+            this.txtEscolaridade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDataNascimento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValorCurso = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtValorDesconto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtValorMulta = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSalvarDados = new System.Windows.Forms.Button();
+            this.txtLista = new System.Windows.Forms.TextBox();
+            this.Cursos = new System.Windows.Forms.Label();
+            this.btnLimparCampos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 32);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(440, 27);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(471, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Curso";
+            // 
+            // txtCurso
+            // 
+            this.txtCurso.Location = new System.Drawing.Point(471, 32);
+            this.txtCurso.Name = "txtCurso";
+            this.txtCurso.Size = new System.Drawing.Size(244, 27);
+            this.txtCurso.TabIndex = 3;
+            // 
+            // txtEscolaridade
+            // 
+            this.txtEscolaridade.Location = new System.Drawing.Point(12, 97);
+            this.txtEscolaridade.Name = "txtEscolaridade";
+            this.txtEscolaridade.Size = new System.Drawing.Size(244, 27);
+            this.txtEscolaridade.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Escolaridade";
+            // 
+            // txtDataNascimento
+            // 
+            this.txtDataNascimento.Location = new System.Drawing.Point(266, 97);
+            this.txtDataNascimento.Name = "txtDataNascimento";
+            this.txtDataNascimento.Size = new System.Drawing.Size(244, 27);
+            this.txtDataNascimento.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Data de Nascimento";
+            // 
+            // txtValorCurso
+            // 
+            this.txtValorCurso.Location = new System.Drawing.Point(12, 165);
+            this.txtValorCurso.Name = "txtValorCurso";
+            this.txtValorCurso.Size = new System.Drawing.Size(94, 27);
+            this.txtValorCurso.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Valor Curso";
+            // 
+            // txtValorDesconto
+            // 
+            this.txtValorDesconto.Location = new System.Drawing.Point(135, 165);
+            this.txtValorDesconto.Name = "txtValorDesconto";
+            this.txtValorDesconto.Size = new System.Drawing.Size(94, 27);
+            this.txtValorDesconto.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(135, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Valor Desconto";
+            // 
+            // txtValorMulta
+            // 
+            this.txtValorMulta.Location = new System.Drawing.Point(266, 165);
+            this.txtValorMulta.Name = "txtValorMulta";
+            this.txtValorMulta.Size = new System.Drawing.Size(94, 27);
+            this.txtValorMulta.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(266, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Valor Multa";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtIdade.Enabled = false;
+            this.txtIdade.Location = new System.Drawing.Point(528, 97);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(94, 27);
+            this.txtIdade.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(528, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Idade";
+            // 
+            // btnSalvarDados
+            // 
+            this.btnSalvarDados.Location = new System.Drawing.Point(12, 213);
+            this.btnSalvarDados.Name = "btnSalvarDados";
+            this.btnSalvarDados.Size = new System.Drawing.Size(856, 29);
+            this.btnSalvarDados.TabIndex = 16;
+            this.btnSalvarDados.Text = "Salvar Dados";
+            this.btnSalvarDados.UseVisualStyleBackColor = true;
+            this.btnSalvarDados.Click += new System.EventHandler(this.btnSalvarDados_Click);
+            // 
+            // txtLista
+            // 
+            this.txtLista.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtLista.Enabled = false;
+            this.txtLista.Location = new System.Drawing.Point(12, 318);
+            this.txtLista.Multiline = true;
+            this.txtLista.Name = "txtLista";
+            this.txtLista.Size = new System.Drawing.Size(850, 146);
+            this.txtLista.TabIndex = 18;
+            // 
+            // Cursos
+            // 
+            this.Cursos.AutoSize = true;
+            this.Cursos.Location = new System.Drawing.Point(12, 295);
+            this.Cursos.Name = "Cursos";
+            this.Cursos.Size = new System.Drawing.Size(107, 20);
+            this.Cursos.TabIndex = 17;
+            this.Cursos.Text = "Lista de Cursos";
+            // 
+            // btnLimparCampos
+            // 
+            this.btnLimparCampos.Location = new System.Drawing.Point(12, 248);
+            this.btnLimparCampos.Name = "btnLimparCampos";
+            this.btnLimparCampos.Size = new System.Drawing.Size(856, 29);
+            this.btnLimparCampos.TabIndex = 19;
+            this.btnLimparCampos.Text = "Limpar Campos";
+            this.btnLimparCampos.UseVisualStyleBackColor = true;
+            this.btnLimparCampos.Click += new System.EventHandler(this.btnLimparCampos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(874, 602);
+            this.Controls.Add(this.btnLimparCampos);
+            this.Controls.Add(this.txtLista);
+            this.Controls.Add(this.Cursos);
+            this.Controls.Add(this.btnSalvarDados);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtValorMulta);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtValorDesconto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtValorCurso);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDataNascimento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEscolaridade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCurso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtNome;
+        private Label label1;
+        private Label label2;
+        private TextBox txtCurso;
+        private TextBox txtEscolaridade;
+        private Label label3;
+        private TextBox txtDataNascimento;
+        private Label label4;
+        private TextBox txtValorCurso;
+        private Label label5;
+        private TextBox txtValorDesconto;
+        private Label label6;
+        private TextBox txtValorMulta;
+        private Label label7;
+        private TextBox txtIdade;
+        private Label label8;
+        private Button btnSalvarDados;
+        private TextBox txtLista;
+        private Label Cursos;
+        private Button btnLimparCampos;
     }
 }
--- FormularioInscricao/Form1.cs
+++ FormularioInscricao/Form1.cs
@@ -6,5 +6,28 @@ namespace FormularioInscricao
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Aqui será o momento de simular a inscrição do aluno
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalvarDados_Click(object sender, EventArgs e)
+        {
+            /*ToDo: Códigos*/
+
+            MessageBox.Show("Olá Mundo !!!");
+            MessageBox.Show($"Nome {txtNome.Text}");
+            txtIdade.Text = "37";
+        }
+
+        private void btnLimparCampos_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtCurso.Text = "";
+            txtEscolaridade.Text = "";
+            txtDataNascimento.Text = "";
+            txtIdade.Text = "";
+        }
     }
 }
